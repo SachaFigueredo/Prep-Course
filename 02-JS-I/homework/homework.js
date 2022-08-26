c9b496480aff6cce84afb508198bd84192181562
Sacha Figueredo <sacha_figueredo@hotmail.com>
2022-08-26 10:32:54 -0300

    segundo commit

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -200,12 +200,16 @@ function obtenerSaludo(nombre) {
   // Toma la string "nombre" y concatena otras string en la cadena para que tome la siguiente forma:
   // "Martin" -> "Hola Martin!"
   // Tu código:
+  var frase = `hola ${nombre}!`
+  return frase;
   
 }
 
 function obtenerAreaRectangulo(alto, ancho) {
   // Retornar el area de un rectángulo teniendo su altura y ancho
   // Tu código:
+  var area = alto + ancho 
+  return area
   
 }
 
@@ -213,14 +217,16 @@ function obtenerAreaRectangulo(alto, ancho) {
 function retornarPerimetro(lado){
   //Escibe una función a la cual reciba el valor del lado de un cuadrado y retorne su perímetro.
   //Escribe tu código aquí
-  
+  var perimetro = lado * 4
+  return perimetro 
 }
 
 
 function areaDelTriangulo(base, altura){
   //Desarrolle una función que calcule el área de un triángulo.
   //Escribe tu código aquí
-
+var area = (base * altura) / 2;
+return area
 }
 
 
@@ -228,17 +234,21 @@ function deEuroAdolar(euro){
   //Supongamos que 1 euro equivale a 1.20 dólares. Escribe un programa que reciba
   //como parámetro un número de euros y calcule el cambio en dólares.
   //Escribe tu código aquí
-  
+  var dolares = euro * 1.2;
+  return dolares
 }
 
 
 function esVocal(letra){
-  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje “Es vocal”. 
+  //Escribe una función que reciba una letra y, si es una vocal, muestre el mensaje "Es vocal". 
   //Verificar si el usuario ingresó un string de más de un carácter, en ese caso, informarle 
   //que no se puede procesar el dato mediante el mensaje "Dato incorrecto".
   // Si no es vocal, tambien debe devolver "Dato incorrecto".
   //Escribe tu código aquí
-  
+  if (letra === "a" || letra === "e" || letra === "i" || letra === "o" || letra === "u")
+  {return "Es vocal"}
+  if (letra.length !== 1) {return "Dato incorrecto"}
+  return "Dato incorrecto"
 }
 
 
